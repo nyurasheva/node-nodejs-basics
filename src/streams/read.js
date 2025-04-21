b0e757425b35bb67ce82039759a1599d69c8c5d4
nyurasheva <n.yurasheva@gmail.com>
2025-04-21 17:12:02 -0600

    feat: add streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+  const readStream = createReadStream(filePath, { encoding: 'utf-8' });
+
+  readStream.on('data', (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  readStream.on('error', (err) => {
+    console.error(`Error reading file: ${err.message}`);
+  });
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const reverseStream = new Transform({
+    transform(chunk, _, callback) {
+      const reversedText = chunk.toString().split('').reverse().join('') + '\n\n';
+      this.push(reversedText);
+      callback();
+    }
+  });
+
+  process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
